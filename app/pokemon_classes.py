d4df2951d97586611ffa84ca65924a299150fd4b
Raphael Torres <raphaeltorresprofissional@gmail.com>
2023-05-16 11:00:28 -0300

    error with property in classes fixed

--- app/pokemon_classes.py
+++ app/pokemon_classes.py
@@ -7,63 +7,68 @@
 # pokemon com vantagem ==> dobro de dano e perde meia vida apenas
 import random
 
-def game_logic():
-    bulbasaur = Grass("bulbasaur", 45, 9, 45)
-    charmander = Fire("charmander", 39, 10, 65)
-    squirtle = Water("squirtle", 44, 8, 43)
 
 class Pokemon():
     def __init__(self, name, life, damage, speed) -> None:
-        self.name = name;
-        self.life = life;
-        self.damage = damage;
-        self.speed = speed;
-        self.advantage = False;
+        self._name = name
+        self._life = life
+        self._damage = damage
+        self._speed = speed
+        self._advantage = False
+
+    @property
+    def name(self):
+        return self._name 
+    
+    @property
+    def life(self):
+        return self._life
+    
+    @property
+    def damage(self):
+        return self._damage
+    
+    @property
+    def speed(self):
+        return self._speed
 
     def attack(self):
-        list_choice = [self.damage, self.damage * 1.5]
+        list_choice = [self._damage, self._damage * 1.5]
         attack = random.choices(list_choice, weights=(90, 10), k=1)
-        if self.advantage:
+        if self._advantage:
             return attack * 2
         return attack
 
     def loose_life(self, damage_received):
-        if self.advantage:
-            self.life -= damage_received / 2
-            return self.life
-        self.life -= damage_received
-        return self.life
+        if self._advantage:
+            self._life -= damage_received / 2
+            return self._life
+        self._life -= damage_received
+        return self._life
 
-    @property
-    def name(self):
-        return self.name
-    
-    @property
-    def life(self):
-        return self.life
-    
-    @property
-    def damage(self):
-        return self.damage
-    
-    @property
-    def speed(self):
-        return self.speed
-    
-    @property
-    def speed(self):
-        return self.speed
 
 class Water(Pokemon):
-    def __init__(self, name, life, damage, speed) -> None:
+    def __init__(self, name, life, damage, speed):
         super().__init__(name, life, damage, speed)
 
 
 class Grass(Pokemon):
-    def __init__(self, name, life, damage, speed) -> None:
+    def __init__(self, name, life, damage, speed):
         super().__init__(name, life, damage, speed)
 
 
 class Fire(Pokemon):
-    def __init__(self, name, life, damage, speed) -> None:
+    def __init__(self, name, life, damage, speed):
         super().__init__(name, life, damage, speed)
+
+
+def game_logic():
+    # CREATING POKEMONS 
+    bulbasaur = Grass("bulbasaur", 45, 9, 45)
+    charmander = Fire("charmander", 39, 10, 65)
+    squirtle = Water("squirtle", 44, 8, 43)
+
+    pokemon_list = [bulbasaur, charmander, squirtle]
+    print(random.choice(pokemon_list).name)
+
+game_logic()
