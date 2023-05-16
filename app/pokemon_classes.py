1d2922f963004d0446dfcd277210cffe56d2d199
Raphael Torres <raphaeltorresprofissional@gmail.com>
2023-05-16 10:37:08 -0300

    getters to pokemon class, fixed loose life logic

--- app/pokemon_classes.py
+++ app/pokemon_classes.py
@@ -1,44 +1,69 @@
 # CARDS: https://www.youtube.com/watch?v=UYI959DTPBE&ab_channel=WebDevPills
+# CHARACTERS INFO: https://pokemondb.net/pokedex/bulbasaur
 
 # Pokemon mais rapido ataca primeiro
 # velocidade igual == aleatorio
 # 10% de chance de acertar crítico (dano dobrado)
 # pokemon com vantagem ==> dobro de dano e perde meia vida apenas
+import random
 
 def game_logic():
-    bulbasaur = Grass("bulbasaur", 45, 8, 45)
+    bulbasaur = Grass("bulbasaur", 45, 9, 45)
+    charmander = Fire("charmander", 39, 10, 65)
+    squirtle = Water("squirtle", 44, 8, 43)
 
 class Pokemon():
-    def __init__(self, name, life, attack, speed) -> None:
+    def __init__(self, name, life, damage, speed) -> None:
         self.name = name;
         self.life = life;
-        self.attack = attack;
+        self.damage = damage;
         self.speed = speed;
         self.advantage = False;
 
     def attack(self):
+        list_choice = [self.damage, self.damage * 1.5]
+        attack = random.choices(list_choice, weights=(90, 10), k=1)
         if self.advantage:
-            return self.attack * 2
-        return self.attack
+            return attack * 2
+        return attack
 
-    def loose_life(self):
+    def loose_life(self, damage_received):
         if self.advantage:
-            self.life -= self.life / 2
+            self.life -= damage_received / 2
             return self.life
-        self.life -= self.life
+        self.life -= damage_received
         return self.life
 
+    @property
+    def name(self):
+        return self.name
+    
+    @property
+    def life(self):
+        return self.life
+    
+    @property
+    def damage(self):
+        return self.damage
+    
+    @property
+    def speed(self):
+        return self.speed
+    
+    @property
+    def speed(self):
+        return self.speed
 
 class Water(Pokemon):
-    def __init__(self, name, life, attack, speed) -> None:
-        super().__init__(name, life, attack, speed)
+    def __init__(self, name, life, damage, speed) -> None:
+        super().__init__(name, life, damage, speed)
 
 
 class Grass(Pokemon):
-    def __init__(self, name, life, attack, speed) -> None:
-        super().__init__(name, life, attack, speed)
+    def __init__(self, name, life, damage, speed) -> None:
+        super().__init__(name, life, damage, speed)
 
 
 class Fire(Pokemon):
-    def __init__(self, name, life, attack, speed) -> None:
-        super().__init__(name, life, attack, speed)
+    def __init__(self, name, life, damage, speed) -> None:
+        super().__init__(name, life, damage, speed)
