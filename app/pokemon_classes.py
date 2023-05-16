74a1cc8db685bb5a22eb71eaff093a1e251f110c
Raphael Torres <raphaeltorresprofissional@gmail.com>
2023-05-16 11:24:28 -0300

    starting basic logic

--- app/pokemon_classes.py
+++ app/pokemon_classes.py
@@ -1,10 +1,6 @@
 # CARDS: https://www.youtube.com/watch?v=UYI959DTPBE&ab_channel=WebDevPills
 # CHARACTERS INFO: https://pokemondb.net/pokedex/bulbasaur
 
-# Pokemon mais rapido ataca primeiro
-# velocidade igual == aleatorio
-# 10% de chance de acertar crítico (dano dobrado)
-# pokemon com vantagem ==> dobro de dano e perde meia vida apenas
 import random
 
 
@@ -34,11 +30,14 @@ class Pokemon():
 
     def attack(self):
         list_choice = [self._damage, self._damage * 1.5]
+        # 10% de chance de acertar crítico (dano dobrado)
         attack = random.choices(list_choice, weights=(90, 10), k=1)
         if self._advantage:
             return attack * 2
         return attack
 
+    
+    # pokemon com vantagem ==> dobro de dano e perde meia vida apenas
     def loose_life(self, damage_received):
         if self._advantage:
             self._life -= damage_received / 2
@@ -50,16 +49,19 @@ class Pokemon():
 class Water(Pokemon):
     def __init__(self, name, life, damage, speed):
         super().__init__(name, life, damage, speed)
+        self.type_attacks = ["Attack 1", "Ataque 2"]
 
 
 class Grass(Pokemon):
     def __init__(self, name, life, damage, speed):
         super().__init__(name, life, damage, speed)
+        self.type_attacks = ["Attack 1", "Ataque 2"]
 
 
 class Fire(Pokemon):
     def __init__(self, name, life, damage, speed):
         super().__init__(name, life, damage, speed)
+        self.type_attacks = ["Attack 1", "Ataque 2"]
 
 
 def game_logic():
@@ -69,6 +71,27 @@ def game_logic():
     squirtle = Water("squirtle", 44, 8, 43)
 
     pokemon_list = [bulbasaur, charmander, squirtle]
-    print(random.choice(pokemon_list).name)
+    computer = random.choice(pokemon_list).name
+
+    person_choice = input("Pokemon name [0b/1c/2s] >> ")
+    if person_choice == "0":
+        player = bulbasaur
+    elif person_choice == "1":
+        player = charmander
+    elif person_choice == "2":
+        player = squirtle
+
+    # Pokemon mais rapido ataca primeiro
+    if computer.speed > player.speed:
+        # computer starts first
+        ...
+    # Velocidade igual == aleatorio
+    elif computer.speed == player.speed:
+        # random start
+        ...
+    else:
+        #player starts first
+        ...
+
 
 game_logic()
