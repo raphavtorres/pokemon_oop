db1070c46bf18a2cc356d7e581dac694dd9e4ba4
Raphael Torres <raphaeltorresprofissional@gmail.com>
2023-05-16 09:27:58 -0300

    getting images and using background

--- app/app.py
+++ app/app.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Hello, World"
+    return render_template(
+        'index.html',
+    )
 
 
 if __name__ == "__main__":
